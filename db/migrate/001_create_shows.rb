f53f588e547448d3b645a705191364212891c725
Annie Truex <artruex@gmail.com>
2019-10-13 04:04:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.0]
-  create_table :shows do |t|
-    t.string = :name 
-    t.string = :network 
-    t.string = :day
-    t.integer = :rating 
-   end
+  def change 
+    create_table :shows do |t|
+      t.string = :name 
+      t.string = :network 
+      t.string = :day
+      t.integer = :rating 
+    end
   end
 end
