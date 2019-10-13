2e6399847780ebdca21550b8e451a65ef07c6c9a
Annie Truex <artruex@gmail.com>
2019-10-13 03:48:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,1 +1,6 @@
-class CreateShows < ActiveRecord::
+class CreateShows < ActiveRecord::Migration[5.0]
+  create_table :shows do |t|
+    t.string = name 
+    t.string = network 
+    t.string = day
+end
